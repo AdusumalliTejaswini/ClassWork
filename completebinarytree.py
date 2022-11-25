48da38781ab611aef942d6371d5c02522275914f
AdusumalliTejaswini <114715860+AdusumalliTejaswini@users.noreply.github.com>
2022-11-25 08:38:50 +0530

    Create completebinarytree.py

---
+++ completebinarytree.py
@@ -0,0 +1,21 @@
+class newNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=self.right=None
+def insertLevelOrder(arr,i,n):
+    root=None
+    if i<n:
+        root=newNode(arr[i])
+        root.left=insertLevelOrder(arr,2*i+1,n)
+        root.right=insertLevelOrder(arr,2*i+2,n)
+    return root
+def inOrder(root):
+    if root!=None:
+        inOrder(root.left)
+        print(root.data,end=" ")
+        inOrder(root.right)
+arr=[1,2,3,4,5,6,6,6,6]
+n=len(arr)
+root=None
+root=insertLevelOrder(arr,0,n)
+inOrder(root)
